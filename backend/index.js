461c7b0cbc975652726db4204e38f7d6c69d6b78
juber <juberk1010@gmail.com>
2025-03-12 11:45:01 +0530

    fixx

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(
   cors({
-    origin: "https://gozeal-pi.vercel.app/",
+    origin: "https://gozeal-pi.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   })
